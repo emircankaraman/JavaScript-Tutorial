2b254a3b02ed341c5394f57aa7153ccbe5892f1f
Emirrcan <emirrcankaraman@gmail.com>
2023-03-08 15:04:28 +0300

    doysa eklendi

--- javascript lessons/Javascript basic/Veriables/script.js
+++ javascript lessons/Javascript basic/Veriables/script.js
@@ -3,3 +3,7 @@ var maascan = 5000;
 console.log(5000 + (5000 * 0.3)); // Maaş Ali
 console.log(5000 + (5000 * 0.3)); // Maaş Can
 
+// Türkçe kelimler kullanmamalıyız.
+// Arada boşluk olmaz.
+// Sayı ile başlamaz.
+
