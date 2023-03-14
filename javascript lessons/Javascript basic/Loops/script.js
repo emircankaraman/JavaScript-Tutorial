d77a6bb4b572a544e1cf6d25001914ffda8df015
Emircan Karaman <102962439+emircankaraman@users.noreply.github.com>
2023-03-14 15:34:01 +0300

    Loops klasörü eklendi

---
+++ javascript lessons/Javascript basic/Loops/script.js
@@ -0,0 +1,53 @@
+//
+// let toplam = 0;
+// for (let i = 1; i<=10;i++){
+//     toplam += i;
+//     console.log(toplam);
+// }
+// console.log("Toplam: "+toplam);
+//
+
+let sayılar = [
+    1,4,5,8,4,3,12,5,3,5
+];
+let toplam = 0;
+
+// for (let i = 0; i<sayılar.length;i++){
+//     console.log(sayılar[i]);
+// }
+
+// for (let index in sayılar){
+//     toplam += sayılar[index];
+// }
+
+// for (let sayi of sayılar){
+//     toplam += sayi
+// }
+
+console.log(toplam);
+
+
+let user ={
+    "name:": " Emircan Karaman",
+    "Usurname:": " emircankaraman",
+    "password:": " emircan1234",
+    "email:": " emirrcankaraman@gmail.com"
+};
+
+for (let key in user){
+    console.log(key + user[key]);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
