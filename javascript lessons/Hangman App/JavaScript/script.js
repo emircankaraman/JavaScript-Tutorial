812441c8030e235bb29f41c892083a857b7aca05
Emircan <emirrcankaraman@gmail.com>
2023-04-22 21:30:06 +0300

    ...

--- javascript lessons/Hangman App/JavaScript/script.js
+++ javascript lessons/Hangman App/JavaScript/script.js
@@ -1,17 +1,22 @@
 const word_el = document.getElementById('word');
-const correctLetters = ['p', 'y', 't', 'h', 'o', 'n',];
+const popup = document.getElementById('popup-container');
+const message_el = document.getElementById('success-message');
+const wrongLetter_el = document.getElementById('wrong-letters');
+const correctLetters = [];
 const wrongLetter = [];
+let selectedWord = getRandomWord();
+const items = document.querySelectorAll('.item');
+const messega = document.getElementById('messege');
+const PlayAgainBtn = document.getElementById('play-again');
 
 
 // Burada ki function bize rastgele bir kelime getirmesini sağlıyor.
 function getRandomWord() {
-    const words = ["javascript", "java", "pyhton"];
+    const words = ["javascrıpt", "java", "pyhton", "css", "html"];
     return words[Math.floor(Math.random() * words.length)];
 }
 
 function displayWord() {
-    // Burada selectedWord'a getRandomWord'u atıyoruz.
-    const selectedWord = getRandomWord()
     word_el.innerHTML = `
     ${selectedWord.split('').map(letter => `
             <div class="letter"> 
@@ -23,8 +28,66 @@ function displayWord() {
 
     const w = word_el.innerText.replace(/\n/g, '');
     if (w === selectedWord) {
-        console.log("tebrikler")
+        popup.style.display = 'flex';
+        message_el.innerText = 'Tebrikler kazandınız.';
     }
 }
+
+function uptadeWrongletter() {
+    wrongLetter_el.innerHTML = `
+    ${wrongLetter.length > 0 ? '<h3>Hatalı Harfler</h3>' : ''}
+    ${wrongLetter.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    items.forEach((item, index) => {
+        const errowCount = wrongLetter.length;
+        if (index < errowCount) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+    if (wrongLetter.length === items.length) {
+        popup.style.display = 'flex';
+        message_el.innerText = 'Maalesef Kaybettiniz.';
+    }
+}
+
+function displayMessega() {
+    messega.classList.add('show');
+    setTimeout(function () {
+        messega.classList.remove('show');
+    }, 2000)
+}
+
+PlayAgainBtn.addEventListener('click', function () {
+    correctLetters.splice(0);
+    wrongLetter.splice(0);
+    selectedWord = getRandomWord();
+    displayWord()
+    uptadeWrongletter();
+    popup.style.display ='none';
+});
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displayWord()
+            } else {
+                displayMessega();
+            }
+        } else {
+            if (!wrongLetter.includes(letter)) {
+                wrongLetter.push(letter);
+                uptadeWrongletter();
+            } else {
+                displayMessega()
+            }
+        }
+    }
+
+});
 
 displayWord()
